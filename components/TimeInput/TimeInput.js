99676bd8d39221e3bd4a3485c4de2a71797b736f
Timmy <timgabrielnguyen@gmail.com>
2019-04-08 00:25:11 -0700

    Set up TimeInput

---
+++ components/TimeInput/TimeInput.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {Text, View, TextInput} from "react-native";
+import PropTypes from "prop-types";
+import styles from "./styles";
+
+export default class TimeInput extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TimeInput</Text>
+      </View>
+    );
+  }
+}
