23b43f5ca291728d40876e95064d3f225b8af31e
Timmy <timgabrielnguyen@gmail.com>
2019-04-10 16:51:14 -0700

    Set State for Secs and Mins of Timer

--- components/TimeInput/TimeInput.js
+++ components/TimeInput/TimeInput.js
@@ -1,9 +1,24 @@
 import React, {Component} from "react";
 import {Text, View, TextInput} from "react-native";
-import PropTypes from "prop-types";
-import styles from "./styles";
+// import PropTypes from "prop-types";
+// import styles from "./styles";
 
 export default class TimeInput extends Component {
+  state = {
+    secs: this.props.value % 60,
+    mins: Math.floor(this.props.value / 60)
+  };
+  getMin = minChange => {
+    const mins = +minChange;
+    this.setState({mins});
+    this.props.onChange(mins * 60 + this.state.secs);
+  };
+
+  getSec = secChange => {
+    const secs = +secChange;
+    this.setState({secs});
+    this.props.onChange(this.state.mins * 60 + secs);
+  };
   render() {
     return (
       <View>
