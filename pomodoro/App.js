3d871b1590f235e45fda8235ebc8ab13d5d8cf69
Timmy <timgabrielnguyen@gmail.com>
2019-04-22 01:13:39 -0700

    Set Up Project with Expo

--- pomodoro/App.js
+++ pomodoro/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
 import TimeInput from "./components/TimeInput";
-import CountDown from "./components/CountDown";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Test</Text>
+        <TimeInput />
       </View>
     );
   }
