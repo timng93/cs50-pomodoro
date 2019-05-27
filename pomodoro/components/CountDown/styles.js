69c6cb1e87a6fef10e399a5a64983fae52564148
Timmy <timgabrielnguyen@gmail.com>
2019-05-26 23:02:49 -0700

    Center Countdown

--- pomodoro/components/CountDown/styles.js
+++ pomodoro/components/CountDown/styles.js
@@ -1,7 +1,7 @@
 import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
-  text: {fontSize: 72}
+  text: {fontSize: 72, textAlign: "center", color: "red"}
 });
 
 export default styles;
